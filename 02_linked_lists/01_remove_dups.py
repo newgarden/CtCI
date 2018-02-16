ccb3621626125cc6c2491aa4c272e8f035fedcd6
Andrey Novosad <andrenewgarden@gmail.com>
2018-02-16 21:02:55 +0300

    Remove Dups

---
+++ 02_linked_lists/01_remove_dups.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Remove dups.
+
+Problem statement: Write code to remove duplicates from an unsorted linked list.
+FOLLOW UP: How would you solve this problem if a temporary buffer is not allowed?
+
+"""
+import unittest
+
+from linked_list import LinkedList
+
+
+def remove_dups1(lst):
+    """
+    Remove duplicates from a linked list using a temporary buffer.
+
+    Takes O(N) time and O(N) additional space. This algorithm can work only with lists containing
+    hashable objects, otherwise we won't be able to use set as a buffer.
+
+    Args:
+        lst (LinkedList): Linked list containing any hashable objects.
+
+    """
+    node = lst.head
+    if not node:
+        return
+
+    values = set()
+    values.add(node.value)
+
+    while node.next:
+        if node.next.value in values:
+            temp = node.next
+            node.next = node.next.next
+            del temp
+        else:
+            node = node.next
+            values.add(node.value)
+
+
+def remove_dups2(lst):
+    """
+    Remove duplicates from a linked list without using additional memory.
+
+    Takes O(N^2) time and O(1) additional space. Unlike the faster algorithm with a buffer this
+    algorithm can work with lists containing arbitrary objects.
+
+    Args:
+        lst (LinkedList): Linked list containing arbitrary objects.
+
+    """
+    current = lst.head
+    while current:
+        runner = current
+        while runner.next:
+            if runner.next.value == current.value:
+                temp = runner.next
+                runner.next = runner.next.next
+                del temp
+            else:
+                runner = runner.next
+        current = current.next
+
+
+class TestRemoveDups(unittest.TestCase):
+    data = [
+        ([], []),
+        ([1], [1]),
+        ([1, 2], [1, 2]),
+        ([1, 1], [1]),
+        (['a', 'a', 'a'], ['a']),
+        (['a', 'b', 'a', 'b'], ['a', 'b']),
+        ([1, 2, 3, 2, 4, 3, 3, 5, 5, 5], [1, 2, 3, 4, 5]),
+        ([1, 1, 2, 1, 2, 2, 3], [1, 2, 3]),
+        ('aaabbcccbbaaa', 'abc'),
+        ([None, 1, 'ab', 1, (1, 2), '1', 'ab', (1, 2), None], [None, 1, 'ab', (1, 2), '1']),
+    ]
+
+    def test_remove_dups1(self):
+        for data in self.data:
+            lst = LinkedList(data[0])
+            remove_dups1(lst)
+            self.assertEqual(lst, LinkedList(data[1]))
+
+    def test_remove_dups2(self):
+        for data in self.data:
+            lst = LinkedList(data[0])
+            remove_dups2(lst)
+            self.assertEqual(lst, LinkedList(data[1]))
+
+
+if __name__ == '__main__':
+    unittest.main()
