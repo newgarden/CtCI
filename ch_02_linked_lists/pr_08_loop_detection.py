79b315aa73b3dd1b463819ace1e4296248202462
Andrey Novosad <a.novosad@cian.ru>
2025-06-17 17:07:59 +0300

    Loop detection

--- ch_02_linked_lists/pr_08_loop_detection.py
+++ ch_02_linked_lists/pr_08_loop_detection.py
@@ -1,54 +1,88 @@
 """
-Loop Detection
+Loop Detection.
 
 Given a circular linked list, implement an algorithm that returns the node at the beginning of
 the loop.
 
-Circular linked list is a linked list in which a node's next pointer points to an earlier
+Circular linked list is a (corrupt) linked list in which a node's next pointer points to an earlier
 node, so as to make a loop in the linked list.
 
 Example:
     Input: A -> B -> C - > D -> E -> C (the same C as earlier)
     Output: C
 
-Hints: #50, #69, #83, #90
-
 """
 import unittest
+
 from .linked_list import LinkedList
+
 
 def detect_loop(lst):
     """
+    Find first loop node in a circular linked list.
+
+    Complexity: O(N) time, O(1) additional space.
+
+    Args:
+        lst (LinkedList): List to check.
+
+    Returns:
+        ListNode: Node at the beginning of the loop. None if there is no loop.
 
     """
     if not lst.head:
         return None
 
-    slow_runner = lst.head.next
-    fast_runner = lst.head.next.next
+    # Let's check if there is a loop, and find any node inside the loop.
+    runner_1 = lst.head
+    runner_2 = lst.head
 
     while True:
-        if not fast_runner:
+        runner_2 = runner_2.next
+        if not runner_2:
             return None
+        if runner_2 is runner_1:
+            break
 
-        slow_runner = slow_runner.next
-        fast_runner = fast_runner.next
-
-        if fast_runner is slow_runner:
-            return fast_runner
-        elif fast_runner is None:
+        runner_2 = runner_2.next
+        if not runner_2:
             return None
+        if runner_2 is runner_1:
+            break
 
-        fast_runner = fast_runner.next
+        runner_1 = runner_1.next
+
+    # Find the number of nodes in the loop
+    loop_length = 1
+    while True:
+        runner_2 = runner_2.next
+        if runner_2 is runner_1:
+            break
+        loop_length += 1
+
+    # Move runner_1 to the head and runner_2 loop_length steps forward from the head
+    runner_1 = lst.head
+    runner_2 = lst.head
+    steps = 1
+    while steps < loop_length:
+        runner_2 = runner_2.next
+        steps += 1
+
+    # Move both runners until runner_1 reaches the beginning of the loop and runner_2 reaches the end.
+    while runner_2.next is not runner_1:
+        runner_2 = runner_2.next
+        runner_1 = runner_1.next
+
+    return runner_1
 
 
-@unittest.skip('Problem is not solved yet.')
 class TestLoopDetection(unittest.TestCase):
 
     def test_loop_detection(self):
-        lst = LinkedList(range(40))
-        for i in range(0, 40):
-            lst[39].next = lst[i]
-            print(i, detect_loop(lst).value)
-
-            # print(i, detect_loop(lst).value)
+        self.assertIsNone(detect_loop(LinkedList()))
+        for i in range(1, 11):
+            lst = LinkedList(range(i))
+            self.assertIsNone(detect_loop(lst))
+            for j in range(i):
+                lst[i - 1].next = lst[j]
+                self.assertIs(detect_loop(lst), lst[j])
