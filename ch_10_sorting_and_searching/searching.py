42c5f5973ae1ad610196a4a063991d82dc9944c4
Andrey Novosad <andrenewgarden@gmail.com>
2025-07-04 16:30:14 +0300

    Binary Search

---
+++ ch_10_sorting_and_searching/searching.py
@@ -0,0 +1,72 @@
+"""Binary search implementation examples."""
+import unittest
+from random import randint
+from typing import Any
+
+
+def binary_search(lst: list, value: Any) -> int | None:
+    """Find a value in a sorted list using binary search.
+
+    Complexity: O(log N) time, O(1) additional space.
+
+    :param lst: Sorted list.
+    :param value: Value to search in the list.
+
+    :return: Index of the value in the list. None if the value is not found in the list.
+    """
+    if not lst:
+        return None
+
+    start = 0
+    if lst[start] == value:
+        return start
+
+    end = len(lst) - 1
+    if lst[end] == value:
+        return end
+
+    while (end - start) > 1:
+        middle = (start + end) // 2
+        if lst[middle] == value:
+            return middle
+        if value < lst[middle]:
+            end = middle
+        else:
+            start = middle
+
+    return None
+
+
+class TestSearching(unittest.TestCase):
+
+    def generate_lists(self):
+        for size in range(50):
+            # For each size generate two lists. One with many repetitions, another with fewer repetitions.
+            lst_1 = []
+            lst_2 = []
+            for i in range(size):
+                lst_1.append(randint(-10, 10))
+                lst_2.append(randint(-150, 150))
+            lst_1.sort()
+            lst_2.sort()
+            yield lst_1
+            yield lst_2
+
+    def test_binary_search(self):
+        for lst in self.generate_lists():
+            for value in lst:
+                with self.subTest((lst, value)):
+                    index = binary_search(lst, value)
+                    self.assertEqual(lst[index], value)
+
+            for i in range(len(lst) - 1):
+                value = (lst[i] + lst[i+1]) // 2
+                if value != lst[i] and value != lst[i + 1]:
+                    with self.subTest((lst, value)):
+                        self.assertIsNone(binary_search(lst, value))
+
+            if lst:
+                with self.subTest((lst, lst[0] - 1)):
+                    self.assertIsNone(binary_search(lst, lst[0] - 1))
+                with self.subTest((lst, lst[-1] + 1)):
+                    self.assertIsNone(binary_search(lst, lst[-1] + 1))
